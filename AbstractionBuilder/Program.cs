32cb750aa63378efd41b1736f7ad8cb76f783a3b
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-09 13:15:08 +0300

    Added basic builder structure

---
+++ AbstractionBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AbstractionBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
