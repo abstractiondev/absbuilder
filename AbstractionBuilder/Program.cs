14d2a45a5b3e71ab2e67d75d69c47221359635e7
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-09 17:58:55 +0300

    Basic tests

--- AbstractionBuilder/Program.cs
+++ AbstractionBuilder/Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TextTemplating;
+using OperationABS.Operation;
 
 namespace AbstractionBuilder
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            CustomCmdLineHost host = new CustomCmdLineHost();
+            host.TemplateFileValue = @"C:\GitHub\kallex\private\Demos\CQRS_CustomerBankAccountDemo\Abstractions\OperationABS\Operation\CSharpCode_v1_0.tt";
+            OperationABS.Operation.CSharpCode_v1_0 generator = new CSharpCode_v1_0();
+            generator.Host = host;
+            string result = generator.TransformText();
+            Console.WriteLine("Generations Done!");
+            return 0;
         }
     }
+
 }
