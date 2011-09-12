02196801e9ce0d0df9027338de255504709425a7
Kalle Launiala <kalle.launiala@protonit.net>
2011-09-12 15:15:16 +0300

    Removed all references and changed T4 to generate its code to content project

--- AbstractionBuilder/Program.cs
+++ AbstractionBuilder/Program.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 //using DocumentationABS.Documentation;
 using Microsoft.VisualStudio.TextTemplating;
-using OperationABS.Operation;
+//using OperationABS.Operation;
 
 namespace AbstractionBuilder
 {
