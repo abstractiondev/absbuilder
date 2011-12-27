8279dce4628a90badb725d373472e544a6d40a83
Kalle Launiala <kalle.launiala@protonit.net>
2011-12-27 11:44:59 +0200

    Initiated T4 + schema include support

--- AbstractionBuilder/Program.cs
+++ AbstractionBuilder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,10 +22,23 @@ namespace AbstractionBuilder
             //string result = generator.TransformText();
             //TransformDocumentation();
             //GenerateDocumentation();
+            if(args != null && args.Length > 0)
+            {
+                return ExecuteProgram(args);
+            }
             Builder builder = new Builder();
             builder.Build();
             Console.WriteLine("Generations Done!");
             return 0;
+        }
+
+        private static int ExecuteProgram(string[] args)
+        {
+            Debugger.Launch();
+            Console.WriteLine("Absbuilder Succeeded 0");
+            Console.Error.WriteLine("Errori virhe!!!");
+            return -1;
+            //return 0;
         }
 
         private static void TransformDocumentation()
@@ -46,6 +60,86 @@ namespace AbstractionBuilder
             }
         }
          * */
+    }
+
+    public class SchemaIncludeSupport
+    {
+        void GenerateXSDSerializer(string templateFile, string outputDirectory, bool generateNamespace, bool generateT4Tags,
+            bool generateToTemplateOutput)
+        {
+            string xsdExeFileName = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles) +
+                                 @"\Microsoft SDKs\Windows\v7.0A\bin\xsd.exe";
+            string xsdFile = GetXSDFile(templateFile);
+            string outputClassFile = outputDirectory + "\\" + System.IO.Path.GetFileNameWithoutExtension(templateFile) + ".cs";
+            string namespaceFileTag = generateNamespace ? "_namespace" : "";
+            string outputttinclude = outputDirectory + "\\" + System.IO.Path.GetFileNameWithoutExtension(templateFile) + namespaceFileTag + ".ttinclude";
+            string outputCsCode = outputDirectory + "\\" + System.IO.Path.GetFileNameWithoutExtension(templateFile) + namespaceFileTag + ".cs";
+            string xsdParameters = "/c \"" + xsdFile + "\" /o:\"" + outputDirectory + "\"";
+            string namespaceName = System.IO.Path.GetFileNameWithoutExtension(templateFile);
+            if (generateNamespace == true && generateT4Tags == false)
+            {
+                xsdParameters = "/n:" + namespaceName + " " + xsdParameters;
+            }
+            System.Diagnostics.ProcessStartInfo processStartInfo = new System.Diagnostics.ProcessStartInfo(xsdExeFileName, xsdParameters);
+            WriteLog("Starting xsd.exe: " + xsdParameters);
+            System.Diagnostics.Process proc = System.Diagnostics.Process.Start(processStartInfo);
+            proc.WaitForExit();
+            WriteLog("System.Diagnostics.Processing include tags...");
+            string content = System.IO.File.ReadAllText(outputClassFile);
+            System.IO.File.Delete(outputClassFile);
+            string outputFile;
+            if (generateT4Tags)
+            {
+                if (generateNamespace)
+                {
+                    content = "<" + "#+" +
+                        System.Environment.NewLine + "public class " + namespaceName + " { " + System.Environment.NewLine +
+                        content.Replace("using System.Xml.Serialization;", "") + "} " + "#" + ">";
+                }
+                else
+                {
+                    content = "<" + "#+" +
+                        System.Environment.NewLine +
+                        content.Replace("using System.Xml.Serialization;", "") + "#" + ">";
+                }
+                outputFile = outputttinclude;
+            }
+            else
+                outputFile = outputCsCode;
+            if (generateToTemplateOutput)
+            {
+                //this.GenerationEnvironment.Clear();
+                //this.GenerationEnvironment.Append(content);
+            }
+            else
+            {
+                WriteLog("Writing to file: " + outputFile);
+                System.IO.File.WriteAllText(outputFile, content);
+            }
+        }
+
+        string GetXSDFile(string templateFile)
+        {
+            string directory = System.IO.Path.GetDirectoryName(templateFile);
+            string xsdFile = directory + "\\" + System.IO.Path.GetFileNameWithoutExtension(templateFile) + ".xsd";
+            return xsdFile;
+        }
+
+        void GenerateTTInclude(string templateFile, string outputDirectory, bool generateNamespace = false, bool generateT4Tags = true,
+            bool generateToTemplateOutput = false)
+        {
+            GenerateXSDSerializer(templateFile, outputDirectory, generateNamespace, generateT4Tags, generateToTemplateOutput);
+        }
+
+        void GenerateTTInclude(string templateFile, bool generateNamespace = false)
+        {
+            GenerateTTInclude(templateFile, System.IO.Path.GetDirectoryName(templateFile), generateNamespace);
+        }
+
+        void WriteLog(string entry)
+        {
+        }
+
     }
 
 }
